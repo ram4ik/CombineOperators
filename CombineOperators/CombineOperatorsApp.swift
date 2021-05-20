31ae66af456f083a74d66f658f37436b2f086039
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-20 08:19:27 +0300

    Initial Commit

---
+++ CombineOperators/CombineOperatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineOperatorsApp.swift
+//  CombineOperators
+//
+//  Created by ramil on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CombineOperatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
